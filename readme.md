c2b520b655f6e0c3b1626ac4e2e7f37ba4871eef
Alma <isakvent@icloud.com>
2026-05-08 15:26:17 +0200

    (initialization): Import project to repository
    Import all progress from local development environment to cloud repository

---
+++ readme.md
@@ -0,0 +1,46 @@
+# Roster Card
+Live link: [midnattlantern.github.io/roster-card](https://midnattlantern.github.io/roster-card/)
+
+## About Roster Card
+Roster card is an experimentation and R&D lineaging from [MultiCo Roster](https://midnattlantern.github.io/multico-roster/), to discover new creative ways to express and present artistic elements for MultiCo Roster through mutable vector graphics. This experimentation serves to support my personal ambition to design engaging user interactions and convince visitors/users through instinct. It's also part of vocational training in graphical design.
+
+My history with graphical design range all the way back to 2017 (2015 with raster art), trained through junior high and self taught curiosity. Up until this project, my vector skills didn't stretch beyond Adobe Illustrator/ Affinity, and static SVG inline or img url import, as well as basic fill manipulation in css. This project dig deeper down the low-end source code of an SVG file, from editing id's and classes, to change the text content of a <text> element in JavaScript.
+
+## Raster asset(s)
+The featured images are a set of drawings pulled from [MultiCo Roster](https://midnattlantern.github.io/multico-roster/). It's been compressed from its lossless PNG-format to a resource friendly webp-format through Affinity. These are the hero images, so they are the main attraction of this project. Because these assets feature transparent pixels, I can test making a backdrop shadow that isn't a square. The width and height of the source material vary in width, height and aspect ratio, which is a problem for a layout design that demand a fixed rule, this can be solved using Affinity's document setup tool and set the width/ height from there.
+
+The grid image is a screenshot of Blender's editor view, an appropriate background for a R&D project experimenting on graphical technicality.
+
+## Vector asset(s)
+
+
+## AI disclosure
+I remain committed to distancing myself from AI and LLMs by minimizing my use of them. Claude may have been involved when looking up technical information, such as TypeScript flags or syntax, Vite documentation, or for grammatical consultation on paragraphs.
+
+Generating a raster image through AI was part of the vocational training programme. This project dodged that task by raising ethical concerns (carbon emissions, uncompensated artist theft, etc.) with the teacher. This is not an outright rejection of generative AI and its inevitable use in professional life, but an act to reduce its use whenever possible, reserving generative AI until it's absolutely necessary while encouraging more ethical methods of creation.
+
+In a theoretical scenario where I would resort to [OpenArt.ai](https://openart.ai/suite/create-image/byte-plus-seedream-4-5), I'd upload [this image](https://se.pinterest.com/pin/415738609356010366/) as reference, and give it the prompt _"Video game splash art, fusing western and east Asian 'anime', floral black pattern on the trenchcoat, add silver plates on the shoulders. Wind flowing toward the figure. Remove the hat, give the figure shoulder-long straight silver hair following the wind. Replace the cane with a naginata with Victorian characteristics, as tall as the figure, the blade pointing to the sky."_ From what I've heard, the more specific the prompt, the better the results, but the outcome could still go either way.
+
+## Other notes
+To access the code of an SVG file in VS code, right click the file in the explorer, select "Open With...", then select "Text Editor".
+
+When using the SVG import as an image source, you're stuck with a static asset that cannot be mutated. Inline enable full customization and access to the path's id's. However, that would make your HTML or JavaScript messy and WET. To get the best of both worlds, Vite can import the SVG as a string, by adding `?raw` at the end of an SVG import. Make an SVG vessel (container) and fill that vessel with the SVG string import. [vite.dev/guide/assets](https://vite.dev/guide/assets)
+``` JavaScript
+// JavaScript
+import mySVG from './assets/mySVG.svg?raw';
+
+const mySVGVessel = document.createElement("div");
+mySVGVessel.innerHTML = mySVG;
+```
+
+Exported SVG files, (As far as I'm aware) will have a default value of width and height of 100%. This means the SVG will be as large or small as its parent/ container.
+``` JavaScript
+// JavaScript
+mySVGVessel.classList.add("my-svg-vessel");
+```
+``` css
+/* css */
+.my-svg-vessel {
+    width: 100px; /* 10px or 10000px, the SVG will scale */
+}
+```
--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,108 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+import gsap from 'gsap';
+import './style.scss'
+import NicoletteSVG from "/nicolette-name.svg?raw";
+import SlantedHeroSVG from "./assets/slanted-hero.svg?raw";
+import SlantedHeroIndexSVG from "./assets/slanted-hero-index.svg?raw";
+import BulletdiamondSVG from "./assets/bulletdiamond.svg?raw";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const splashArtWidth: number = 350;
+const splashArtGapWidth: number = 50;
+let currentSplashArtIndex: 0 | 1 | 2 = 0;
 
-<div class="ticks"></div>
+const viewport = document.getElementById("main");
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+const splashArtFrame = document.createElement("figure");
+splashArtFrame.classList.add("splash-art-frame");
+splashArtFrame.id = "splashArtFrame";
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// ==========
+// Splash art
+// ==========
+const splashArt1 = document.createElement("img");
+splashArt1.src = "./nicolette-compressed-1.webp";
+splashArt1.id = "splashArt1";
+splashArt1.classList.add("splash-art");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const splashArt2 = document.createElement("img");
+splashArt2.src = "./nicolette-compressed-2.webp";
+splashArt2.id = "splashArt2";
+splashArt2.classList.add("splash-art");
+
+const splashArt3 = document.createElement("img");
+splashArt3.src = "./nicolette-compressed-3.webp";
+splashArt3.id = "splashArt3";
+splashArt3.classList.add("splash-art");
+
+splashArtFrame.append(splashArt1, splashArt2, splashArt3);
+// ==========
+
+// ====================
+// Splash art selectors
+// ====================
+const splashArtSelectors = document.createElement("div");
+splashArtSelectors.classList.add("splash-art-selectors");
+
+const splashArtButton1 = document.createElement("button");
+splashArtButton1.textContent = "splash hero 0";
+splashArtButton1.addEventListener("click", () => {
+    handleScrollSplashArt(0);
+})
+
+const splashArtButton2 = document.createElement("button");
+splashArtButton2.textContent = "splash hero 1";
+splashArtButton2.addEventListener("click", () => {
+    handleScrollSplashArt(1);
+})
+
+const splashArtButton3 = document.createElement("button");
+splashArtButton3.textContent = "splash hero 2";
+splashArtButton3.addEventListener("click", () => {
+    handleScrollSplashArt(2);
+})
+
+splashArtSelectors.append(splashArtButton1, splashArtButton2, splashArtButton3);
+// ====================
+
+// ===
+// SVG
+// ===
+const svgNicoletteBanner = document.createElement("div");
+svgNicoletteBanner.classList.add('svg-nicolette-banner');
+svgNicoletteBanner.innerHTML = NicoletteSVG;
+
+const slantedStripeHeroWrapper = document.createElement("div");
+slantedStripeHeroWrapper.classList.add("slanted-stripe-hero-wrapper");
+
+const svgHeroContainer = document.createElement("div");
+svgHeroContainer.innerHTML = SlantedHeroSVG;
+
+const svgHeroIndexContainer = document.createElement("div");
+svgHeroIndexContainer.innerHTML = SlantedHeroIndexSVG;
+let slantedHeroIndexValue: null | SVGTextContentElement = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    slantedHeroIndexValue = document.querySelector<SVGTextElement>("#slanted-hero-index-value");
+    if (!slantedHeroIndexValue) return;
+    slantedHeroIndexValue.textContent = currentSplashArtIndex.toString();
+});
+
+const bulletdiamond = document.createElement("div");
+bulletdiamond.innerHTML = BulletdiamondSVG;
+
+slantedStripeHeroWrapper.append(svgHeroContainer, svgHeroIndexContainer, bulletdiamond);
+// ===
+
+viewport?.append(splashArtFrame, splashArtSelectors, svgNicoletteBanner, slantedStripeHeroWrapper);
+
+function handleScrollSplashArt(setIndex: 0 | 1 | 2 ) {
+    currentSplashArtIndex = setIndex;
+    gsap.to(".splash-art", {
+        x: ((splashArtWidth + splashArtGapWidth) * -currentSplashArtIndex),
+        duration: 0.3
+    });
+    gsap.to("#bulletdiamond", {
+        x: "100%"
+    })
+    if (!slantedHeroIndexValue) return;
+    slantedHeroIndexValue.textContent = setIndex.toString();
+}
